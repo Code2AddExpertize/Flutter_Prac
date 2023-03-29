bdcedbce830d4f998469c58f4f33a46f3b124542
@Code2AddExpertize <code2addexpertize@gmail.com>
2023-03-30 05:28:20 +0530

    Flutter custom theme and Style

---
+++ Component/flutter_style_and_theme/lib/ui_helper/text_style.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'dart:ui';
+
+TextStyle mTextStyle11(
+    {Color textColor = Colors.black, FontWeight fontWeight = FontWeight.bold}) {
+  return TextStyle(
+    fontSize: 11,
+    color: textColor,
+  );
+}
