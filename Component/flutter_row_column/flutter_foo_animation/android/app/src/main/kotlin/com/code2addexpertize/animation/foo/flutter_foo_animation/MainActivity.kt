8e6482c5ae8d09769cc7c4c3fd6c15a3934c9ae3
@Code2AddExpertize <code2addexpertize@gmail.com>
2023-04-08 17:26:45 +0530

    Foo Animation and Opacity Animation Location Changed

---
+++ Component/flutter_row_column/flutter_foo_animation/android/app/src/main/kotlin/com/code2addexpertize/animation/foo/flutter_foo_animation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.code2addexpertize.animation.foo.flutter_foo_animation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Component/flutter_row_column/flutter_opacity_animation/android/app/src/main/kotlin/com/code2addexperitze/animation/opacity/flutter_opacity_animation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.code2addexperitze.animation.opacity.flutter_opacity_animation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
