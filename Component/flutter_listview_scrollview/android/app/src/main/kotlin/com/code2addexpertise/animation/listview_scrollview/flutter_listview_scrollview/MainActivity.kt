21b8cd8393a9c1fffa731838a9e627bd29ddc022
@Code2AddExpertize <code2addexpertize@gmail.com>
2023-04-10 15:16:27 +0530

    Flutter ListWheel Scrollview Proj

---
+++ Component/flutter_listview_scrollview/android/app/src/main/kotlin/com/code2addexpertise/animation/listview_scrollview/flutter_listview_scrollview/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.code2addexpertise.animation.listview_scrollview.flutter_listview_scrollview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
