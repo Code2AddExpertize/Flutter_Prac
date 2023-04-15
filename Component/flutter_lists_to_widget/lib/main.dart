e66d0ea5f09f8a9800bd0c77c3e13254039c50c2
@Code2AddExpertize <code2addexpertize@gmail.com>
2023-04-15 22:30:29 +0530

    Project changes

--- Component/flutter_lists_to_widget/lib/main.dart
+++ Component/flutter_lists_to_widget/lib/main.dart
@@ -1,4 +1,11 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_lists_to_widget/colors_list.dart';
+import 'package:flutter_lists_to_widget/StringUtils.dart';
+import 'package:flutter_lists_to_widget/material/material_colors.dart';
+
+import 'material/material_color_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter List to Widget'),
     );
   }
 }
@@ -29,33 +36,43 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var list = [
-    {'name': 'Ashish', 'mobno': '0123456789', 'unread': 2},
-    {'name': 'Chadni', 'mobno': '0123456789', 'unread': 2},
-    {'name': 'khushboo', 'mobno': '0123456789', 'unread': 2},
-    {'name': 'Mummy', 'mobno': '0123456789', 'unread': 2},
-    {'name': "Papa", 'mobno': '0123456789', 'unread': 2},
-    {'name': "Preeti", 'mobno': '0123456789', 'unread': 2}
-  ];
 
+
+
+    var colorList = ColorList();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
+      // body: ListView(
+      //     children: colorList.colorList.asMap().entries .map((entries) {
+      //       return ListTile(
+      //         leading: Icon(Icons.color_lens),
+      //         title: Text('${colorList.colorListString[entries.key].toString().replaceAll('Colors.', '').toString().capitalize()}',style: TextStyle(fontWeight: FontWeight.bold),),
+      //         subtitle: Text('Color Code:#${entries.value.value.toRadixString(16)}'),
+      //         trailing: CircleAvatar(
+      //             radius: 31,
+      //             backgroundColor: entries.value,
+      //             child: Text('')),
+      //       );
+      //
+      //     }).toList()
+      // ),
       body: ListView(
-        children: list.map((index) {
-          return ListTile(
-            leading: Icon(Icons.person),
-            title: Text('${index['name']}'),
-            subtitle: Text('${index['mobno']}'),
-            trailing: CircleAvatar(
-                radius: 11,
-                backgroundColor: Colors.green,
-                child: Text('${index['unread']}')),
-          );
-        }).toList(),
+        children: MaterialColorlist.colorList.asMap().entries .map((entries) {
+            return ListTile(
+              leading: Icon(Icons.color_lens),
+              title: Text('${MaterialColorlist.colorListString[entries.key].toString()}',style: TextStyle(fontWeight: FontWeight.bold),),
+              subtitle: Text('Color Code:#${entries.value.value.toRadixString(16)}'),
+              trailing: CircleAvatar(
+                  radius: 31,
+                  backgroundColor: entries.value,
+                  child: Text('')),
+            );
+
+        }).toList()
       ),
     );
   }
